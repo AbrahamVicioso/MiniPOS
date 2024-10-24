4c22a34fc0078e3e07394ffd2d33dbc4751aa0f0
Abraham Vicioso <abrahanvicioso@gmail.com>
2024-10-24 12:19:40 -0400

    ADD: BLEEZER AUTH

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col">
+    <label class="font-medium text-blue-500 text-2xl text-200%] m-0">MiniPos</label>
+    <p class="text-sm text-[60%] mt-0">A minimal POS created by Abraham Vicioso</p>
+</div>
